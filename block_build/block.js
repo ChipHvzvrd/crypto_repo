22906f41c5b934b1cc68ccc4823ebdc326eda9e5
chiphvzvrd <christianipena@yahoo.com>
2023-03-28 15:38:34 -0500

    genesis block

--- block_build/block.js
+++ block_build/block.js
@@ -5,4 +5,26 @@ class Block {
         this.hash = hash;
         this.data = data;
     }
+
+    toString() {
+        return `Block -
+        Timestamp: ${this.timestamp} 
+        Last Hash: ${this.lastHash.substring(0, 10)}
+        Hash     : ${this.hash.substring(0, 10)}
+        Data     : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('init time', '-----', 'f1r57-h45h', []);
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = 'todo-hash';
+
+        return new this(timestamp, lastHash, hash, data);
+    }
 }
+
+module.exports = Block;
