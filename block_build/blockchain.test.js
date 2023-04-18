71c912470b3fedf25695e46f63c2c7653a3b66d4
chiphvzvrd <christianipena@yahoo.com>
2023-04-18 17:13:00 -0500

    replace chain function and validate chain test

--- block_build/blockchain.test.js
+++ block_build/blockchain.test.js
@@ -4,10 +4,11 @@ const Block = require('./block');
 
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('start with genesis block', () => {
@@ -20,4 +21,37 @@ describe('Blockchain', () => {
 
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
     });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('foo');
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('validates a chain with a corrupted genesis block', () => {
+        bc2.chain[0].data = 'Badf data';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not foo';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('goo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('does not replace the chain with one of less than or equal to length', () => {
+        bc.addBlock('foo');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).not.toEqual(bc2.chain);
+    })
 });
